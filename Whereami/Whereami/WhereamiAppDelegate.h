d6e3e45aa9e7dd64d0a92b0cafd8f6b96a8dd361
FangYiXiong <haydenfang@icloud.com>
2014-04-17 20:10:23 +0800

    init

---
+++ Whereami/Whereami/WhereamiAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  WhereamiAppDelegate.h
+//  Whereami
+//
+//  Created by FangYiXiong on 14-4-17.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface WhereamiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Whereami/Whereami/WhereamiViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhereamiViewController.h
+//  Whereami
+//
+//  Created by FangYiXiong on 14-4-17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhereamiViewController : UIViewController
+
+@end
