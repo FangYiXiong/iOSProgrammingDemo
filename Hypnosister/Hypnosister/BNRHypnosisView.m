09dcf3b4826e6dd3f0797c78eb05fc5d5e75a071
FangYiXiong <haydenfang@icloud.com>
2014-04-20 10:43:53 +0800

    绘制一个⭕️

--- Hypnosister/Hypnosister/BNRHypnosisView.m
+++ Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -19,13 +19,31 @@
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
+
 - (void)drawRect:(CGRect)rect
 {
-    // Drawing code
+    CGRect bounds = self.bounds;
+    
+    // 根据 bounds，计算中心点
+    CGPoint center;
+    center.x = bounds.origin.x + bounds.size.width / 2.0;
+    center.y = bounds.origin.y + bounds.size.height / 2.0;
+    
+    // 计算圆的半径，使其大小能够尽可能地撑满整个视图
+    float radius = (MIN(bounds.size.width, bounds.size.height) / 2.0);
+    
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    
+    // 增加一个 0 到 2*PI 的一个圆弧（就是一个圆）
+    [path addArcWithCenter:center
+                    radius:radius
+                startAngle:0
+                  endAngle:M_PI * 2.0
+                 clockwise:YES];
+    
+    // 绘制图形！
+    [path stroke];
 }
-*/
+
 
 @end
