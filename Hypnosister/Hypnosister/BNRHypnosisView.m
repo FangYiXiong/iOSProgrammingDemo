629ad9781bf9d22b3b075c003a3db0862852bc54
FangYiXiong <haydenfang@icloud.com>
2014-04-20 11:02:23 +0800

    画了一个麦田怪圈－－

--- Hypnosister/Hypnosister/BNRHypnosisView.m
+++ Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -30,17 +30,22 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
-    // 计算圆的半径，使其大小能够尽可能地撑满整个视图
-    float radius = (MIN(bounds.size.width, bounds.size.height) / 2.0);
-    
+    // 计算最大圆的半径，hypot(double,double) 返回直角三角形斜边的长度
+    float maxRadius = (hypot(bounds.size.width, bounds.size.height) / 2.0);
+
     UIBezierPath *path = [[UIBezierPath alloc] init];
     
-    // 增加一个 0 到 2*PI 的一个圆弧（就是一个圆）
-    [path addArcWithCenter:center
-                    radius:radius
-                startAngle:0
-                  endAngle:M_PI * 2.0
-                 clockwise:YES];
+    for (float currentRadius = maxRadius; currentRadius > 0 ; currentRadius -= 20) {
+        // 画一系列的同心圆
+        [path addArcWithCenter:center
+                        radius:currentRadius
+                    startAngle:0
+                      endAngle:M_PI * 2.0
+                     clockwise:YES];
+    }
+    
+    
+
     
     // 配置线的宽度为 10 points.
     path.lineWidth = 10;
