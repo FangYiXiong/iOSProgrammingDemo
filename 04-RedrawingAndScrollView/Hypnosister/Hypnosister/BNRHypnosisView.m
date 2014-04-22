1e9ba9c8876a8f3189d7d26a024da9e490811af2
FangYiXiong <haydenfang@icloud.com>
2014-04-22 10:28:44 +0800

    处理点击事件并让圆圈变色.

--- 04-RedrawingAndScrollView/Hypnosister/Hypnosister/BNRHypnosisView.m
+++ 04-RedrawingAndScrollView/Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -69,5 +69,24 @@
     [path stroke];
 }
 
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    NSLog(@"%@ was touched", self);
+    
+    // 获得 3 个 0 到 1 的浮点型随机数
+    float red = (arc4random() % 100) / 100.0;
+    float green = (arc4random() % 100) / 100.0;
+    float blue = (arc4random() % 100) / 100.0;
+    
+    UIColor *randomColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+    self.circleColor = randomColor;
+}
+
+
+- (void)setCircleColor:(UIColor *)circleColor{
+    _circleColor = circleColor;
+    // 实验 setNeedDisplayInRect 方法，只重画一小部分的 view
+    [self setNeedsDisplayInRect:CGRectMake(0, 0, 200, 100)];
+}
+
 
 @end
